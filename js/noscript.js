e6ef0fa02031cbc698ef501bb4528e04f80b08fc
Shreenidhi Koppal <87005236+Satyamurthi@users.noreply.github.com>
2025-08-07 14:16:32 +0530

    Create noscript.js

---
+++ js/noscript.js
@@ -0,0 +1,27 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      requestAnimationFrame(updateCount);
+    } else {
+      counter.innerText = target.toLocaleString();
+    }
+  };
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        updateCount();
+        observer.unobserve(counter); // Run only once
+      }
+    });
+  }, { threshold: 1 });
+
+  observer.observe(counter);
+});
